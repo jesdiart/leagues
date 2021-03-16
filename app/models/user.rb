8176ae4d3e07ba211e1ddefbe426ddedaadd31ca
Jesús Díaz Artiaga <jesus.diaz@howler.events>
2021-03-16 15:53:44 +0100

    fix user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,9 @@
 class User < ApplicationRecord
+    belongs_to :player
     has_many :leagues, :foreign_key => 'manager_id'
     has_many :teams, :foreign_key => 'coordinator_id'
 
     validates :username, presence: true
-    validates :first_name, presence: true
-    validates :last_name, presence: true
     validates :password, presence: true
     validates :password, confirmation: { case_sensitive: true }
-    validates :email, presence: true
 end
